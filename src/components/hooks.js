acbe30c5fc24e17591018d959562bc34824edcf8
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2019-04-25 12:08:55 +0200

    working map with infovindows

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -1,5 +1,18 @@
 import { useEffect, useState } from "react";
 import GoogleMapsApiLoader from "google-maps-api-loader";
+import database from "../data/db";
+
+function importAll(r) {
+  let images = {};
+  r.keys().map(item => {
+    return (images[item.replace("./", "")] = r(item));
+  });
+  return images;
+}
+
+const images = importAll(
+  require.context("../../public/img/cont", false, /\.(png|jpe?g|svg)$/)
+);
 
 // Google Mapのオブジェクトを呼び出すだけのhooks
 export const useGoogleMap = apiKey => {
@@ -22,25 +35,82 @@ export const useMap = ({ googleMap, mapContainerRef, initialConfig }) => {
     }
     const map = new googleMap.maps.Map(mapContainerRef.current, initialConfig);
     setMap(map);
-    const marker = new googleMap.maps.Marker({
-      position: { lat: 49.0308605, lng: 17.3423464 },
-      map: map,
-      title: "Hello World!"
-    });
-    const infoWindow = new googleMap.maps.InfoWindow();
+    const markersData = database;
 
-    googleMap.maps.event.addListener(marker, "click", function() {
-      map.setZoom(13);
-      map.setCenter(marker.getPosition());
-      // Creating the content to be inserted in the infowindow
-      let iwContent = `<div class="info_content">text</div>`;
+    displayMarkers();
+    // This function will iterate over markersData array
+    // creating markers with createMarker function
+    function displayMarkers() {
+      // this variable sets the map bounds according to markers position
+      let bounds = new googleMap.maps.LatLngBounds();
 
-      // including content to the Info Window.
-      infoWindow.setContent(iwContent);
+      // for loop traverses markersData array calling createMarker function for each marker
+      markersData.forEach(element => {
+        let latlng = new googleMap.maps.LatLng(element.lat, element.lng);
+        let name = element.name;
+        let picture = element.picture;
+        let address = element.address;
+        let postalCode = element.postalCode;
+        let phoneNumber = element.phoneNumber;
+        let website = element.website;
+        createMarker(
+          latlng,
+          name,
+          picture,
+          address,
+          postalCode,
+          phoneNumber,
+          website
+        );
+        console.log(element);
+        // marker position is added to bounds variable
+        bounds.extend(latlng);
+      });
 
-      // opening the Info Window in the current map and at the current marker location.
-      infoWindow.open(map, marker);
-    });
+      // Finally the bounds variable is used to set the map bounds
+      // with fitBounds() function
+      map.fitBounds(bounds);
+    }
+    function createMarker(
+      latlng,
+      name,
+      picture,
+      address,
+      postalCode,
+      phoneNumber,
+      website
+    ) {
+      const marker = new googleMap.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "Hello World!"
+      });
+      const infoWindow = new googleMap.maps.InfoWindow();
+
+      googleMap.maps.event.addListener(map, "click", function() {
+        infoWindow.close();
+      });
+
+      googleMap.maps.event.addListener(marker, "click", function() {
+        map.setZoom(13);
+        map.setCenter(marker.getPosition());
+        // Creating the content to be inserted in the infowindow
+        let iwContent = `<div class="info_content">  <img
+        class="info_picture"
+        src=${picture}
+        alt=${name}
+      /><div class="info_title">${name}</div>
+        <div class="info_adress">${address}</div><div class="info_postal">
+            ${postalCode}</div>
+          <div class="info_telephone">${phoneNumber}</div><div class="info_link">${website}</div></div>`;
+
+        // including content to the Info Window.
+        infoWindow.setContent(iwContent);
+
+        // opening the Info Window in the current map and at the current marker location.
+        infoWindow.open(map, marker);
+      });
+    }
   }, [googleMap, initialConfig, mapContainerRef]); // initialConfigは変わったとしても再マウントするとおかしなことになるので更新対象にしない // googleMapかmapContainerRefが変化したらeffectが発火する。
 
   // あとで使えるようにmapを返すようにする
