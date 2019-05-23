6b8d2160948a210810d22c93cfc72fae5715d0cd
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2019-05-23 14:10:16 +0200

    display more button added

--- src/pages/Prodejny.js
+++ src/pages/Prodejny.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 //import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Navigation from "../components/Navigation";
 import ShopsList from "../components/ShopsList";
@@ -6,10 +7,30 @@ import database from "../data/db";
 import "../App.css";
 
 function Prodejny() {
+  let [initialArray, setInitialArray] = useState([]);
+  let [dataIndex, setDataIndex] = useState(8);
+  const displayMore = () => {
+    if (database.length < dataIndex) return;
+    setDataIndex(dataIndex + 8);
+    let updateArray = [];
+    for (let i = 0; i < dataIndex; i++) {
+      updateArray.push(database[i]);
+    }
+    setInitialArray(updateArray);
+  };
+  console.log(initialArray);
+
   return (
     <div className="main__container">
       <Navigation />
-      <ShopsList shops={database} />
+      <div className="shops__box">
+        <div className="more__box">
+          <button className="more__button" onClick={() => displayMore()}>
+            More
+          </button>
+        </div>
+        <ShopsList shops={initialArray} displayMore={displayMore} />
+      </div>
     </div>
   );
 }
