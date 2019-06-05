a2cebc4ccef8144915e43d8a0e8499d9ded60845
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2019-06-05 17:15:46 +0200

    fixed imgs, css layout, transitions

--- src/components/FilteredCellar.js
+++ src/components/FilteredCellar.js
@@ -16,7 +16,7 @@ const FilteredCellar = ({ item }) => {
   return (
     <article className="wine_container">
       <div className="wine_box">
-        <h4 className="pic_caption">{item.name}</h4>
+        <h4 className="pic__caption">{item.name}</h4>
         <img
           className="front_picture"
           src={images[item.picture]}
--- src/components/FilteredCity.js
+++ src/components/FilteredCity.js
@@ -19,7 +19,7 @@ const FilteredCity = ({ item, updateModal }) => {
 
   return (
     <div className="city_box">
-      <h4 className="pic_caption">{item.name}</h4>
+      <h4 className="pic__caption">{item.name}</h4>
       <img
         className="front_picture"
         src={images[item.picture]}
